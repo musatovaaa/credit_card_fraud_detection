23ee350c8de1de67b5a6dea234a63ca0185e9cbf
Natasha Musatova <natali.schimpl@gmail.com>
2024-11-26 15:35:12 +0300

    Small changes

--- src/settings.py
+++ src/settings.py
@@ -15,9 +15,32 @@ if not os.path.isdir(PREDICTIONS_DIR):
 # TRACKING_URI = settings.MLFLOW_TRACKING_URI
 
 # columns
-DROP_FEATURES = ["V14", "V16", "V21", "V23", "V24", "V26", "Time"]
+# drop features "V14", "V16", "V21", "V23", "V24", "V26", "Time"
+FEATURES = [
+    "V1",
+    "V2",
+    "V3",
+    "V4",
+    "V5",
+    "V6",
+    "V7",
+    "V8",
+    "V9",
+    "V10",
+    "V11",
+    "V12",
+    "V13",
+    "V15",
+    "V17",
+    "V18",
+    "V19",
+    "V20",
+    "V22",
+    "V25",
+    "V27",
+    "V28",
+    "Amount",
+]
 TARGET = ["Class"]
-
-MODELS_LIST = ["Encoder", "Boosting", "RandomForest"]
-
+MODELS_LIST = ["Boosting", "RandomForest", "Encoder"]
 SAMPLING_STRATEGY = 0.5

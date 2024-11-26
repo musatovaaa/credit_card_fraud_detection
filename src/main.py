3903a904327c9ef3394b018f2326cdc0e85e1b66
Natasha Musatova <natali.schimpl@gmail.com>
2024-11-26 15:29:55 +0300

    Add argument parser

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
 import mlflow
 import argparse
-from datetime import datetime
 
-from src.train import ModelTrainer
 from src.test import ModelTester
-
-# from src.predict import ModelPredictor
-from src import settings
+from src.train import ModelTrainer
+from src.predict import ModelPredictor
 
 # mlflow.set_tracking_uri(settings.TRACKING_URI)
 
@@ -17,20 +14,20 @@ def main():
     parser.add_argument("--predict", action="store_true")
     args = parser.parse_args()
 
-    # if args.train:
-    # experiment_name = "Antifraud test"
-    # mlflow.set_experiment(experiment_name)
-    # with mlflow.start_run(run_name=datetime.now().strftime("%Y-%m-%d %H:%M")):
-    models, test = ModelTrainer().train_all_models()
-    ModelTester(models, test).test_all_models()
+    if args.train:
+        # experiment_name = "Antifraud test"
+        # mlflow.set_experiment(experiment_name)
+        # with mlflow.start_run(run_name=datetime.now().strftime("%Y-%m-%d %H:%M")):
+        models, test = ModelTrainer().train_all_models()
+        ModelTester(models, test).test_all_models()
 
-    # elif args.predict:
-    # experiment_name = "Antifraud predict"
-    # mlflow.set_experiment(experiment_name)
-    # ModelPredictor().predict_all_models()
+    elif args.predict:
+        # experiment_name = "Antifraud predict"
+        # mlflow.set_experiment(experiment_name)
+        ModelPredictor().predict_all_models()
 
-    # else:
-    # parser.print_help()
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
